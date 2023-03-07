f033ba04bb9b4a558713211cd499ff2fceb0e24e
coding-madmonkey <jwhmk225@gmail.com>
2023-03-07 22:03:13 +0900

    여러가지 추가

--- src/subscriber_test.cpp
+++ src/subscriber_test.cpp
@@ -1,3 +1,4 @@
+//ds4 test
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Int8.h>
@@ -20,3 +21,52 @@ void subCallback(const ds4_driver::Status::ConstPtr testmsg){
   std::cout << value << std::endl;
 }
 
+//int sub test
+/*
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Int8.h>
+
+void subCallback(const std_msgs::Int8::ConstPtr testmsg);
+
+int main(int argc, char** argv){
+  ros:: init(argc,argv,"test_node");
+  ros::NodeHandle nh;
+  ros::Subscriber testsub=nh.subscribe("test",10,subCallback);
+
+  //std_msgs::Int8 pub_msg;
+  //ros::Publisher testpub = nh.advertise<std_msgs::Int8>("test", 10);
+  //testpub.publish(pub_msg);
+
+  ros::spin();
+
+}
+
+void subCallback(const std_msgs::Int8::ConstPtr testmsg){
+  int value=testmsg->data;
+  if(value==1){printf("value is 1\n");}
+  if(value==2){printf("value is 2\n");}
+}
+*/
+
+//default code
+/*
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+  ros::spin();
+  return 0;
+}
+*/
